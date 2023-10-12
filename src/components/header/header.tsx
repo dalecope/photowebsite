9912c844aaedd2d1222cbbc38d08164d5af2412f
dalecope <dalecope@hotmail.com>
2023-10-11 22:46:47 -0400

    changed title from Cinematt to DianeDale

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -24,7 +24,7 @@ const Header = ({ className, navRevealed, onMenuButtonClick, onTitleClick }: Pro
       ) : (
         <Link href="/">
           <Title data-testid="title" onClick={onTitleClick}>
-            Cinematt
+            DianeDale
           </Title>
         </Link>
       )}
